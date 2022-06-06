ac4bb2116a732aa1d0cf3b8d6ef33f337993c35f
이종상 <helius96@naver.com>
2022-06-06 23:55:11 +0900

    small

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-const title = document.querySelector("div.hello:first-child h1")
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    console.log("title was clicked")
+    h1.style.color = "blue";
 }
 
-title.addEventListener("click", handleTitleClick);
+function handleMouseEnter(){
+    h1.innerText = "Mouse is Here!";
+}
 
-handleTitleClick()
+function handleMouseLeave(){
+    h1.innerText = "Mouse is Gone!";
+}
+
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+//h1.onclick = handleTitleClick;
+h1.addEventListener("click", handleTitleClick);
+//h1.onmouseenter = handleMouseEnter;
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
