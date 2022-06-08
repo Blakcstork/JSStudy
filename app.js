be93a4394d453729e44ad0faff9c6b97f3293c42
이종상 <helius96@naver.com>
2022-06-08 23:32:17 +0900

    no

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
+const loginForm = documnet.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick(){
+
+function onLoginSubmit(event){
+
+    event.preventDefault();
 
     const username = loginInput.value;
     console.log(username);
-    /*
-    if(username === ""){
-        alert("Please write your name!");
-    }
-    else if(username.length > 15){
-        alert("username is too long!");
-    }
-    else{
-        console.log(loginInput.value);
-    }
-    */
-    
+
 }
 
-loginButton.addEventListener("click",onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+
