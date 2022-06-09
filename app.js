8bc5e4b5bd6a4c6b97399b49e89aca5f92d4ed7b
이종상 <helius96@naver.com>
2022-06-09 22:59:36 +0900

    savings

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
-const loginForm = documnet.querySelector("#login-form")
+const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
+
+
+const link = document.querySelector("a");
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)//show the form 
+}
 
 
 function onLoginSubmit(event){
-
     event.preventDefault();
-
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+
+
+    localStorage.setItem(USERNAME_KEY, username);
     console.log(username);
+    paintGreetings(username);
 
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event)
+}
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME)//show the form
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else{
+    paintGreetings(savedUsername);
+}
+
+link.addEventListener("click", handleLinkClick);
