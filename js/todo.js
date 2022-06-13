29bd65efa5a5a41ca73f76bf71f3ffd081150c42
이종상 <helius96@naver.com>
2022-06-13 23:07:08 +0900

    chi

--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("todos",toDos);
+}
 
 
-function deleteToDo(){
+
+function deleteToDo(event){
+    console.log(event.target.parentElement);
+    const li = event.target.parentElement;
+    li.remove();
+    const hateChinese = 100;
+
 
 }
 
@@ -27,7 +38,9 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newToDo);
     paintToDo(newToDo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit)
